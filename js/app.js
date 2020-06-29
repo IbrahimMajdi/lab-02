a4aa2f888016712bac86d3a9b35472198f7fc951
IbrahimMajdi <ibrmaj1@gmail.com>
2020-06-30 00:11:03 +0300

    filter task

--- js/app.js
+++ js/app.js
@@ -6,25 +6,22 @@ const ajaxSettings = {
 }
 
 
-
 $.ajax('./data/page-1.json', 'ajaxSettings')
     .then(data => {
             data.forEach(item => {
                 let newImg = new Imgs(item.image_url, item.title, item.description, item.keyword, item.horns);
-                // newImg.render();
-                newImg.add();
-                console.log(Imgs.all);
-                
+                newImg.render();
+                Imgs.all.push(item.keyword);
+
             })
+
+            select(Imgs.all);
         }
 
     )
-// console.log('leng', selectors.length);
 
 
 Imgs.all = [];
-
-let y = [5, 7, 8]
 
 function Imgs(imgurl, title, desc, keyword, horns) {
 
@@ -33,49 +30,50 @@ function Imgs(imgurl, title, desc, keyword, horns) {
     this.desc = desc;
     this.keyword = keyword;
     this.horns = horns;
-    // Imgs.all.push(this.keyword);
 }
-
-console.log('all', Imgs.all);
 
 Imgs.prototype.render = function () {
 
     let photo = $('.photo-template').clone();
+    photo.attr('id', this.keyword);
 
     photo.find('h2').text(this.title);
     photo.find('img').attr('src', this.imgurl);
     photo.find('img').attr('alt', this.title);
     photo.find('p').text(this.desc);
     $('.cont').append(photo);
+
     photo.removeClass('photo-template');
 
 }
 
-Imgs.prototype.add = function () {
+// not working propably ????
+$(".select").change(function () {
 
-    Imgs.all.push(this.keyword);
-}
-
+    let val =$(".select option:selected").text();
+    console.log('sel',val);
+    
+    // if (){}
+        // $(".select option:selected").hide();
+        // console.log(this);
+    
+    
+    
+});
 
 
 function select(selectors) {
 
-    // console.log(selectors);
+    const unique = Array.from(new Set(selectors));
 
-    selectors.forEach(item => {
-
-        console.log('1321');
+    unique.forEach(item => {
 
         let opt = $('.opt').clone();
         opt.text(item);
+
         $('.select').append(opt);
+        var val = $('.opt').text();
         opt.removeClass('opt');
 
-
     })
-
 }
-
-console.log('pro',Imgs.all.length);
-
-select(Imgs.all);
