bdfc275732adf5173530d8cffe0bce7164fa2a3f
IbrahimMajdi <ibrmaj1@gmail.com>
2020-06-29 16:23:06 +0300

    feature 2

--- js/app.js
+++ js/app.js
@@ -4,40 +4,66 @@ const ajaxSettings = {
     method: 'get',
     datatype: 'json'
 }
+var selectors = [];
 
 $.ajax('./data/page-1.json', 'ajaxSettings')
     .then(data => {
 
             data.forEach(item => {
-                let newImg = new Imgs(item.image_url, item.tiltle, item.description, item.keyword, item.horns);
-                // console.log(newImg);
+                let newImg = new Imgs(item.image_url, item.title, item.description, item.keyword, item.horns);
                 newImg.render();
+                selectors.push(item.horns);
 
             })
+            
         }
 
     )
 
-function Imgs(imgurl, tiltle, desc, keyword, horns) {
+
+
+function Imgs(imgurl, title, desc, keyword, horns) {
+
     this.imgurl = imgurl;
-    this.tiltle = tiltle;
+    this.title = title;
     this.desc = desc;
     this.keyword = keyword;
     this.horns = horns;
 }
 
+
 Imgs.prototype.render = function () {
 
     let photo = $('.photo-template').clone();
-    //  $('.cont').append(photo);
 
-
-    // photo.removeClass('photo-template');
-
-    // let x =photo.find('h2').text(this.keyword);
-    // let urlImg= photo.find('img').attr('src').text(this.image_url);
-    // $('.photo-template').append(x);
-
-
+    photo.find('h2').text(this.title);
+    photo.find('img').attr('src', this.imgurl);
+    photo.find('img').attr('alt', this.title);
+    photo.find('p').text(this.desc);
+    $('.cont').append(photo);
+    photo.removeClass('photo-template');
 
 }
+
+function select(arr) {
+    console.log(arr.length);
+    
+    for (let i = 0; i < arr.length; i++) {
+        console.log("jo");
+        
+        
+    }
+    // arr.forEach(item => {
+
+    //     console.log('arr');
+        // let opt = $('.opt').clone();
+        // opt.text(item);
+        // $('.select').append(opt);
+        // opt.removeClass('opt');
+
+
+    // })
+
+}
+
+select(selectors);
